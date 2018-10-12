e8cd247f37cece705a0cd70c4dea428e9117e07f
Bernard Adanlessossi <bernard.adanlessossi@t2b.ch>
2018-10-12 09:46:14 +0200

    Added the Cognito Configuration and Amplify

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from "react-router-dom";
+import Amplify from "aws-amplify";
+import config from "./config";
 
 import './index.css';
 import App from './App';
+
+Amplify.configure({
+    Auth: {
+      mandatorySignIn: true,
+      region: config.cognito.REGION,
+      userPoolId: config.cognito.USER_POOL_ID,
+      identityPoolId: config.cognito.IDENTITY_POOL_ID,
+      userPoolWebClientId: config.cognito.APP_CLIENT_ID
+    }
+  });
 
 ReactDOM.render(
     <Router>
