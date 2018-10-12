2d79ac4482b51d720a5487cc94fbf3ee9d0d85bc
Bernard Adanlessossi <bernard.adanlessossi@t2b.ch>
2018-10-12 09:58:16 +0200

    Added Session to the state

--- src/App.js
+++ src/App.js
@@ -7,7 +7,23 @@ import './App.css';
 import Routes from "./Routes";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+  
+    this.state = {
+      isAuthenticated: false
+    };
+  }
+  
+  userHasAuthenticated = authenticated => {
+    this.setState({ isAuthenticated: authenticated });
+  }
+
   render() {
+    const childProps = {
+      isAuthenticated: this.state.isAuthenticated,
+      userHasAuthenticated: this.userHasAuthenticated
+    };
     return (
       <div className="App container">
         <Navbar fluid collapseOnSelect>
@@ -28,7 +44,7 @@ class App extends Component {
             </Nav>
           </Navbar.Collapse>
         </Navbar>
-        <Routes />
+        <Routes childProps={childProps} />
       </div>
     );
   }
--- src/Routes.js
+++ src/Routes.js
@@ -3,12 +3,12 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import PageNotFound from "./components/PageNotFound"
 import Login from "./components/Login"
+import AppliedRoute from "./routes/AppliedRoute"
 
-export default () =>
+export default ({ childProps }) =>
   <Switch>
-    <Route path="/" exact component={Home} />
-    <Route path="/login" exact component={Login} />
-
+    <AppliedRoute path="/" exact component={Home} props={childProps} />
+    <AppliedRoute path="/login" exact component={Login} props={childProps} />
     { /* Finally, catch all unmatched routes */ }
     <Route component={PageNotFound} />
   </Switch>;
