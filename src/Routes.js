739070c3e0237d6e05d53bf954fec4772f063631
Bernard Adanlessossi <bernard.adanlessossi@t2b.ch>
2018-10-12 09:49:59 +0200

    Added The Login Page

--- src/Routes.js
+++ src/Routes.js
@@ -2,10 +2,12 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import PageNotFound from "./components/PageNotFound"
+import Login from "./components/Login"
 
 export default () =>
   <Switch>
     <Route path="/" exact component={Home} />
+    <Route path="/login" exact component={Login} />
 
     { /* Finally, catch all unmatched routes */ }
     <Route component={PageNotFound} />
