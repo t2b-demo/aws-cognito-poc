e0878833b81dfa2687c31ee45be22b83f721fe03
Bernard Adanlessossi <bernard.adanlessossi@t2b.ch>
2018-10-12 10:28:36 +0200

    Added the Signup page

--- src/Routes.js
+++ src/Routes.js
@@ -3,12 +3,15 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import PageNotFound from "./components/PageNotFound"
 import Login from "./components/Login"
+import Signup from "./components/Signup"
 import AppliedRoute from "./routes/AppliedRoute"
 
 export default ({ childProps }) =>
   <Switch>
     <AppliedRoute path="/" exact component={Home} props={childProps} />
     <AppliedRoute path="/login" exact component={Login} props={childProps} />
+    <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
+    
     { /* Finally, catch all unmatched routes */ }
     <Route component={PageNotFound} />
   </Switch>;
