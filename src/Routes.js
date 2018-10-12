a17078d4a53c7bdd90839d8dbb9e77e64532ee26
Bernard Adanlessossi <bernard.adanlessossi@t2b.ch>
2018-10-12 09:38:31 +0200

    Added The Page Not Found Component

--- src/Routes.js
+++ src/Routes.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
+import PageNotFound from "./components/PageNotFound"
 
 export default () =>
   <Switch>
     <Route path="/" exact component={Home} />
+
+    { /* Finally, catch all unmatched routes */ }
+    <Route component={PageNotFound} />
   </Switch>;
