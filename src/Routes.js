e372f5886f038199050c91395fa7bb34bc07d5a3
Bernard Adanlessossi <bernard.adanlessossi@t2b.ch>
2018-10-16 15:37:04 +0200

    Added secure page

--- src/Routes.js
+++ src/Routes.js
@@ -4,13 +4,17 @@ import Home from "./components/Home";
 import PageNotFound from "./components/PageNotFound"
 import Login from "./components/Login"
 import Signup from "./components/Signup"
+import AuthPage from "./components/AuthPage"
 import AppliedRoute from "./routes/AppliedRoute"
+import AuthenticatedRoute from "./routes/AuthenticatedRoute";
+import UnauthenticatedRoute from "./routes/UnauthenticatedRoute";
 
 export default ({ childProps }) =>
   <Switch>
     <AppliedRoute path="/" exact component={Home} props={childProps} />
-    <AppliedRoute path="/login" exact component={Login} props={childProps} />
-    <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
+    <UnauthenticatedRoute  path="/login" exact component={Login} props={childProps} />
+    <UnauthenticatedRoute  path="/signup" exact component={Signup} props={childProps} />
+    <AuthenticatedRoute  path="/authPage" exact component={AuthPage} props={childProps} />
     
     { /* Finally, catch all unmatched routes */ }
     <Route component={PageNotFound} />
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,13 +2,54 @@ import React, { Component } from "react";
 import "./Home.css";
 
 export default class Home extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: true
+    };
+  }
+
+  async componentDidMount() {
+    if (!this.props.isAuthenticated) {
+      return;
+    }
+  
+    try {
+      this.setState({ isLoading: true });
+    } catch (e) {
+      alert(e);
+    }
+  
+    this.setState({ isLoading: false });
+  }
+
+  renderLanding() {
     return (
       <div className="Home">
         <div className="landing">
           <h1>Cognito Demo</h1>
           <p>A simple Login app</p>
         </div>
+      </div>
+    );
+  }
+
+  renderAuthPage() {
+    return (
+      <div className="AuthPage">
+      <div className="landing">
+        <h1>Cognito Authentication Demo</h1>
+        <p>Consider yourself happy if you could see this...</p>
+      </div>
+    </div>
+    );
+  }
+
+  render() {
+    return (
+      <div className="Home">
+        {this.props.isAuthenticated ? this.renderAuthPage() : this.renderLanding()}
       </div>
     );
   }
